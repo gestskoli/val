834696d7ce9fb8ce1641aeafd597211cf7fdf0fa
geir <ges@tskoli.is>
2018-02-23 21:50:17 +0000

    nafnabreytingar

--- main.js
+++ main.js
@@ -1,82 +1,75 @@
-let afangar = [];
+const afangar = [];
 
-let mainDiv = document.getElementById('maingrid');
+const mainDiv = document.getElementById('maingrid');
 
-function saekjaJson() {
+(function saekjaJson() {
     fetch("afangar.json")
         .then(res => res.json())
         .then(json => {
-            for (let j of json) {
-                afangar.push(new Afangi(j));
-            }
-
-            for (let afangi of afangar) {
-                parents = afangi.parents
-                if (parents.length > 0) {
-                    newParents = [];
-                    for (let p of parents) {
-                        for (let afs of afangar) {
-                            if (p === afs.id) {
-                                newParents.push(Object.assign({}, afs));
-                            }
-                        }
+            json.map(j => afangar.push(new Afangi(j)));
+            afangar.map(afangi => {
+                const undanfarar = afangi.parents
+                if (undanfarar.length) {
+                    const undanfararAfanga = [];
+                    for (const undanfari of undanfarar) {
+                        undanfararAfanga.push(
+                            afangar.find(a => a.id === undanfari)
+                        );
                     }
-                    afangi.parents = newParents.slice(0);
+                    afangi.parents = undanfararAfanga.slice(0);
                 }
-            }
-            synaGognDiv();
-        }).catch(e => console.log("villa", e));
-}
-saekjaJson();
+                synaDiv(afangi);
+            });
+            
+            teiknaUndanfara();
+        })
+        .catch(e => console.log("villa:", e));
+})();
 
-function synaGognDiv() {
-    for (a of afangar) {
-        const div = document.createElement('div');
-        div.className = "afangar";
-        div.className += a.tbr ? "" : " nontbr";
-        div.className += a.core ? " core" : "";
-        div.className += ` ${a.id.substring(0,4)}`;
-        div.style.gridArea = a.id.split('0')[0];
-        div.id = a.id;
-        div.innerHTML = a.div;
-        mainDiv.appendChild(div);
-    }
-    teiknaUndanfara();
+function synaDiv(afangi) {
+    const div = document.createElement('div');
+    div.className = "afangar";
+    div.className += afangi.tbr ? "" : " nontbr";
+    div.className += afangi.core ? " core" : "";
+    div.className += ` ${afangi.id.substring(0,4)}`;
+    div.style.gridArea = afangi.id.split('0')[0];
+    div.id = afangi.id;
+    div.innerHTML = afangi.div;
+    mainDiv.appendChild(div);
 }
 
 function teiknaUndanfara() {
-    for (a of afangar) {
-        let curDiv = document.getElementById(a.id);
-        let lines = [];
-        if (a.parents) {
-            for (u of a.parents) {
-                let line = new LeaderLine(
-                    document.getElementById(u.id),
+    for (const afangi of afangar) {
+        const curDiv = document.getElementById(afangi.id);
+        const undanfaraLinur = [];
+        if (afangi.parents) {
+            for (const undanfari of afangi.parents) {
+                const undanfaraLina = new LeaderLine(
+                    document.getElementById(undanfari.id),
                     curDiv, {
                         hide: true
                     }
                 );
-                line.color = "rgba(45, 52, 54,1.0)";
-                line.size = 4;
-                line.endPlug = "disc";
-                line.startPlug = "disc";
-                if (a.id === 'VAL05') {
-                    line.endPlug = "arrow1"
-                    line.setOptions({
+                undanfaraLina.color = "rgba(45, 52, 54,1.0)";
+                undanfaraLina.size = 4;
+                undanfaraLina.endPlug = "disc";
+                undanfaraLina.startPlug = "disc";
+                if (afangi.id === 'VAL05') {
+                    undanfaraLina.endPlug = "arrow1"
+                    undanfaraLina.setOptions({
                         startSocket: 'top'
                     });
                 }
-
-                lines.push(line);
+                undanfaraLinur.push(undanfaraLina);
             }
         }
 
         curDiv.addEventListener("mouseover", e => {
-            for (b of afangar) {
-                if (b.id === e.target.id) {
-                    curDiv.title = b.description;
-                    curDiv.dataTooltip = b.description;
-                    for (l of lines) {
+            for (const afangi of afangar) {
+                if (afangi.id === e.target.id) {
+                    curDiv.title = afangi.description;
+                    curDiv.dataTooltip = afangi.description;
+                    for (l of undanfaraLinur) {
                         l.show("draw");
                     }
                 }
@@ -84,9 +77,9 @@ function teiknaUndanfara() {
         });
 
         curDiv.addEventListener("mouseleave", e => {
-            for (b of afangar) {
-                if (b.id === e.target.id) {
-                    for (l of lines) {
+            for (const afangi of afangar) {
+                if (afangi.id === e.target.id) {
+                    for (l of undanfaraLinur) {
                         l.hide();
                     }
                 }
